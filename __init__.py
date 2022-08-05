3f620348a6aedae73df6b705d449adb1aa5c889f
Niamh Smith <92368623+nsmith29@users.noreply.github.com>
2022-08-05 17:15:56 +0100

    FromFile

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from FromFile.FromInputFile import Kinds, ChargeStateIdentification, NameOfProject
+from FromFile.FromXYZFile import LastXYZ, NumTotAtomsOfKind
+from FromFile.FromLogFile import GetChargesSpins
+from FromFile.ChargeStates4Projects import SortingChargeStates
